63d258753d0628a5a019e5b2031e40d4ce530e7a
Vinicius Patrinhani <vpatrinhani@gmail.com>
2015-06-26 17:41:02 -0300

    AppVeyor scripts MSBuild scripts

--- Build.cmd
+++ Build.cmd
@@ -1,3 +1,6 @@
 @ECHO OFF
 
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild "%~dp0\%1.msbuild" /t:%2 %3 /v:minimal /maxcpucount /nodeReuse:false
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild "%~dp0\%1.msbuild" /t:%2 %3 /v:n /maxcpucount /nodeReuse:false
+
+rem for Debug purposes
+rem %SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild "%~dp0\%1.msbuild" /t:%2 %3 /fl /flp:logfile=%~dp0\%1.msbuild.log /maxcpucount /nodeReuse:false
